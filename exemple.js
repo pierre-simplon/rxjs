0676c080fa4f42f54688638728c7b49e52c42b68
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-12-27 13:42:06 +0100

    interval observable

--- exemple.js
+++ exemple.js
@@ -1,19 +1,5 @@
-import { from } from "rxjs";
-import "babel-polyfill";
+import { interval } from "rxjs";
 
-function* hello() {
-  yield "Hello";
-  yield "World";
-}
+const timer$ = interval(1000);
 
-const iterator = hello();
-
-const observer = {
-  next: (value) => console.log("next", value),
-  error: (error) => console.log("error", error),
-  complete: () => console.log("complete"),
-};
-
-const source$ = from(iterator);
-
-source$.subscribe(observer);
+timer$.subscribe(console.log);
