025cfbc879f87b735176d135ed8713f997b8371f
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-12-28 11:54:17 +0100

    Add mapTo

--- exemple.js
+++ exemple.js
@@ -1,5 +1,5 @@
 import { fromEvent } from "rxjs";
-import { map, pluck } from "rxjs/operators";
+import { map, pluck, mapTo } from "rxjs/operators";
 
 const keyup$ = fromEvent(document, "keyup");
 
@@ -7,6 +7,10 @@ const keycode$ = keyup$.pipe(map((event) => event.code));
 
 const keycodeWithPlug$ = keyup$.pipe(pluck("code"));
 
+const keyPressed$ = keycode$.pipe(mapTo("Keypressed !"));
+
 keycode$.subscribe(console.log);
 
 keycodeWithPlug$.subscribe(console.warn);
+
+keyPressed$.subscribe(console.error);
