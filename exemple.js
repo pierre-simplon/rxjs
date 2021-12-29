88e3bf912e977d7723867ffc40766f8795fed2f4
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-12-29 13:58:55 +0100

    logging with tap

--- exemple.js
+++ exemple.js
@@ -1,5 +1,5 @@
 import { interval } from "rxjs";
-import { scan, mapTo, filter } from "rxjs/operators";
+import { scan, mapTo, filter, tap } from "rxjs/operators";
 
 const countdown = document.getElementById("countdown");
 const message = document.getElementById("message");
@@ -10,6 +10,7 @@ counter$
   .pipe(
     mapTo(-1),
     scan((acc, curr) => acc + curr, 10),
+    tap(console.log),
     filter((val) => val >= 0)
   )
   .subscribe((value) => {
