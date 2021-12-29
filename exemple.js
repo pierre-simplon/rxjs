167f775ec198b4a5c0a8fe12c63ed82ea666772b
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-12-29 13:33:41 +0100

    scan from array

--- exemple.js
+++ exemple.js
@@ -1,11 +1,11 @@
-import { interval } from "rxjs";
-import { reduce, take } from "rxjs/operators";
+import { from } from "rxjs";
+import { scan } from "rxjs/operators";
 
-const numbers$ = interval([1, 2, 3, 4, 5], 1000);
+const numbers$ = from([1, 2, 3, 4, 5]);
 
 const totalReducer = (acc, curr) => acc + curr;
 
-const total$ = numbers$.pipe(take(3), reduce(totalReducer, 0));
+const total$ = numbers$.pipe(scan(totalReducer, 0));
 
 total$.subscribe({
   next: console.log,
