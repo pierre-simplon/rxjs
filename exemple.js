6f5824186e99e2e5c1acb8aa897149bfd24a7da1
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-12-29 05:49:31 +0100

    of filter

--- exemple.js
+++ exemple.js
@@ -1,16 +1,6 @@
-import { fromEvent } from "rxjs";
-import { map, pluck, mapTo } from "rxjs/operators";
+import { of } from "rxjs";
+import { filter } from "rxjs/operators";
 
-const keyup$ = fromEvent(document, "keyup");
-
-const keycode$ = keyup$.pipe(map((event) => event.code));
-
-const keycodeWithPlug$ = keyup$.pipe(pluck("code"));
-
-const keyPressed$ = keycode$.pipe(mapTo("Keypressed !"));
-
-keycode$.subscribe(console.log);
-
-keycodeWithPlug$.subscribe(console.warn);
-
-keyPressed$.subscribe(console.error);
+of(1, 2, 3, 4, 5)
+  .pipe(filter((value) => value > 2))
+  .subscribe(console.log);
