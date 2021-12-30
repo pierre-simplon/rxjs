e76b865430c3fe5efc1d74407a60accee5d541a8
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-12-30 06:39:28 +0100

    take of

--- exemple.js
+++ exemple.js
@@ -1,21 +1,9 @@
-import { interval } from "rxjs";
-import { scan, mapTo, filter, tap } from "rxjs/operators";
+import { of } from "rxjs";
+import { take } from "rxjs/operators";
 
-const countdown = document.getElementById("countdown");
-const message = document.getElementById("message");
+const numbers$ = of(1, 2, 3, 4, 5);
 
-const counter$ = interval(1000);
-
-counter$
-  .pipe(
-    mapTo(-1),
-    scan((acc, curr) => acc + curr, 10),
-    tap(console.log),
-    filter((val) => val >= 0)
-  )
-  .subscribe((value) => {
-    countdown.innerHTML = value;
-    if (!value) {
-      message.innerHTML = "Lift Off!";
-    }
-  });
+numbers$.pipe(take(3)).subscribe({
+  next: console.log,
+  complete: () => console.log("complete"),
+});
