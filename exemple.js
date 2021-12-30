d1421b9d8685f7844c118ad007d8c3e4693a2629
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-12-30 13:02:34 +0100

    debounce with intervall

--- exemple.js
+++ exemple.js
@@ -1,10 +1,14 @@
-import { fromEvent } from "rxjs";
-import { debounceTime, distinctUntilChanged, pluck } from "rxjs/operators";
+import { fromEvent, interval } from "rxjs";
+import { debounce, distinctUntilChanged, pluck } from "rxjs/operators";
 
 const inputBox = document.getElementById("text-input");
 
 const input$ = fromEvent(inputBox, "keyup");
 
 input$
-  .pipe(debounceTime(1000), pluck("target", "value"), distinctUntilChanged())
+  .pipe(
+    debounce(() => interval(1000)),
+    pluck("target", "value"),
+    distinctUntilChanged()
+  )
   .subscribe(console.log);
