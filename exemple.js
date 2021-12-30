9268736659d3368b896f25ce74d08cafaf4c463e
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-12-30 12:56:42 +0100

    Add distinctUntilChanged

--- exemple.js
+++ exemple.js
@@ -1,10 +1,10 @@
 import { fromEvent } from "rxjs";
-import { debounceTime, pluck } from "rxjs/operators";
+import { debounceTime, distinctUntilChanged, pluck } from "rxjs/operators";
 
 const inputBox = document.getElementById("text-input");
 
 const input$ = fromEvent(inputBox, "keyup");
 
 input$
-  .pipe(debounceTime(1000), pluck("target", "value"))
+  .pipe(debounceTime(1000), pluck("target", "value"), distinctUntilChanged)
   .subscribe(console.log);
