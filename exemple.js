c1e90a79bfe47d6505fbf3066edec1006527d01a
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-12-31 06:18:22 +0100

    interval sample fromEvent click

--- exemple.js
+++ exemple.js
@@ -1,14 +1,8 @@
-import { fromEvent } from "rxjs";
-import { sampleTime, map } from "rxjs/operators";
+import { fromEvent, interval } from "rxjs";
+import { sample } from "rxjs/operators";
 
 const click$ = fromEvent(document, "click");
 
-click$
-  .pipe(
-    sampleTime(4000),
-    map(({ clientX, clientY }) => ({
-      clientX,
-      clientY,
-    }))
-  )
-  .subscribe(console.log);
+const timer$ = interval(1000);
+
+timer$.pipe(sample(click$)).subscribe(console.log);
