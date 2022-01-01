ae907be337ac9f66c188d49f0adf436acf7a502c
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2022-01-01 12:15:26 +0100

    exhaustMap interval take 3

--- exemple.js
+++ exemple.js
@@ -1,8 +1,8 @@
 import { fromEvent, interval } from "rxjs";
-import { concatMap, take } from "rxjs/operators";
+import { exhaustMap, take } from "rxjs/operators";
 
 const timer$ = interval(1000).pipe(take(3));
 
 const click$ = fromEvent(document, "click");
 
-click$.pipe(concatMap(() => timer$)).subscribe(console.log);
+click$.pipe(exhaustMap(() => timer$)).subscribe(console.log);
